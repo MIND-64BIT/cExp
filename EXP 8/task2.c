e494897a924c16278439654d6aec050c32a20b22
MIND-64BIT <178900363+MIND-64BIT@users.noreply.github.com>
2024-11-28 21:28:54 +0530

    Update task2.c

--- EXP 8/task2.c
+++ EXP 8/task2.c
@@ -4,7 +4,7 @@ Task: 2
 Name: Prajapati Nitesh
 UIN: 241P006
 Roll no: 06 Division: C
-Title: WAP to find sum of two matrix.
+Title: WAP to check if enterd string is palindrome or not
 */
 
 #include <stdio.h>
